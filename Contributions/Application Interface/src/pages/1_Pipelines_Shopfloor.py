e623be4b84bad212803d9d1adb81ca697de7d86b
ArchLYnuxDev <168268869+ArchLYnuxDev@users.noreply.github.com>
2025-02-17 09:48:19 +0800

    Add files via upload

---
+++ Contributions/Application Interface/src/pages/1_Pipelines_Shopfloor.py
@@ -0,0 +1,52 @@
+import streamlit as st
+from streamlit_flow import streamlit_flow
+from streamlit_flow.elements import StreamlitFlowNode, StreamlitFlowEdge
+from streamlit_flow.state import StreamlitFlowState
+
+import time
+import numpy as np
+
+st.set_page_config(
+  page_title="Shopfloor Demo",
+  page_icon=""
+)
+
+if 'flow_state' not in st.session_state:
+    nodes = [StreamlitFlowNode("1", (0, 0), {'content': 'Node 1'}, 'input', 'right')]
+    edges = []
+    globals()["flow_state"] = StreamlitFlowState(nodes, edges)
+
+st.sidebar.header("Completion Progress")
+st.markdown(    r"""
+    # Pipeline Demo
+    A demonstration!
+""")
+
+progress_bar = st.sidebar.progress(0)
+status_text = st.sidebar.empty()
+
+"""
+for i in range(1, 101):
+    new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+    status_text.text("%i%% Complete" % i)
+    chart.add_rows(new_rows)
+    progress_bar.progress(i)
+    last_rows = new_rows
+    time.sleep(0.05)
+
+progress_bar.empty()
+"""
+
+flow_state = streamlit_flow('flow',
+    flow_state,
+    fit_view=True, 
+    enable_node_menu=True,
+    enable_edge_menu=True,
+    enable_pane_menu=True,
+    get_edge_on_click=True,
+    get_node_on_click=True, 
+    show_minimap=True, 
+    hide_watermark=True, 
+    allow_new_edges=True,
+    min_zoom=0.0
+)
